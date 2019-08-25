63a1bc172c59dc1b5eadcbf1a513c9a139263d96
Michael J. Bondra <mjbondra@gmail.com>
2019-08-24 20:11:05 -0700

    update build file to allow custom version via flag

--- build.sh
+++ build.sh
@@ -6,23 +6,56 @@ CI_LIB="lib"
 CI_LIB_RELATIVE_BIN="../lib"
 CI_PREFIX="ci"
 
-IMAGE_VERSION="$(git tag | wondermonger-version \
-  --version-file .version \
-  --prefix v \
-  --new-version patch)"
+VALID_VERSION_REGEX="^[A-Za-z]*[0-9]+\.[0-9]+\.[0-9]+(-[0-9A-Za-z.-]*[0-9A-Za-z]+)*$"
 
-TPL_CI_IMAGE_HEADER="image: wondermonger/ci-tools:${IMAGE_VERSION}"
-TPL_NODE_BASE="${TPL_CI_IMAGE_HEADER}"$'\n'"$(tail -n +2 ./tpl/node/base.yml)"
-echo "$TPL_NODE_BASE" > ./tpl/node/base.yml
 
+# bin filename functions
 get_relative_path () { echo "${1//$CI_LIB/$CI_LIB_RELATIVE_BIN}"; }
 remove_extension () { echo "${1//.sh/}"; }
 replace_prefix () { echo "${1//$CI_LIB/$CI_PREFIX}"; }
 replace_slash () { echo "${1//\//-}"; }
 
+
+# parse arguments
+while getopts 'v:' FLAG; do
+  case "$FLAG" in
+    v)
+      RAW_IMAGE_VERSION=${OPTARG/v/}
+      if ! [[ "$RAW_IMAGE_VERSION" =~ $VALID_VERSION_REGEX ]]
+      then
+        echo "\"${RAW_IMAGE_VERSION}\" is an invalid semantic version" >&2
+        exit 1
+      fi
+      IMAGE_VERSION="v${RAW_IMAGE_VERSION}"
+      ;;
+    *)
+      echo "Unexpected option $FLAG" >&2
+      ;;
+  esac
+done
+
+
+# version templates
+NEXT_PATCH_VERSION="$(git tag | wondermonger-version \
+  --version-file .version \
+  --prefix v \
+  --new-version patch)"
+
+IMAGE_VERSION=${IMAGE_VERSION:-$NEXT_PATCH_VERSION}
+TPL_CI_IMAGE_HEADER="image: wondermonger/ci-tools:${IMAGE_VERSION}"
+TPL_NODE_BASE="${TPL_CI_IMAGE_HEADER}"$'\n'"$(tail -n +2 ./tpl/node/base.yml)"
+echo "$TPL_NODE_BASE" > ./tpl/node/base.yml
+
+
+# ensure script permissions 
 chmod -R 755 "$CI_LIB"
+
+
+# create temporary bin directory
 mkdir -p "$CI_BIN_TMP"
 
+
+# create script symlinks in temporary bin directory
 while read -r -d "" CI_SCRIPT
 do
   CI_SCRIPT_ALIAS="$(replace_prefix "$(remove_extension "$(replace_slash "$CI_SCRIPT")")")"
@@ -32,5 +65,7 @@ do
   ln -s "$CI_SCRIPT_RELATIVE_PATH" "$CI_SCRIPT_ALIAS_PATH"
 done < <(find lib -name "*.sh" -print0)
 
+
+# replace bin directory with temporary bin directory
 rm -rf "$CI_BIN"
 mv "$CI_BIN_TMP" "$CI_BIN"
